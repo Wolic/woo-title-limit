3bbf3e96567cf2281450d05fdb5c16e08eded05d
Dimitri Wolf <git@dimitri-wolf.de>
2020-01-26 16:53:33 +0100

    new branch for v2 started

---
+++ includes/class-woo-title-limit-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.dimitri-wolf.de
+ * @since      1.0.0
+ *
+ * @package    Woo_Title_Limit
+ * @subpackage Woo_Title_Limit/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Woo_Title_Limit
+ * @subpackage Woo_Title_Limit/includes
+ * @author     Dima W. <wtl@dimitri-wolf.de>
+ */
+class Woo_Title_Limit_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
