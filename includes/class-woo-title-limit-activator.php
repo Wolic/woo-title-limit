9add975f66f149f250461a426f45438fd35cfcd0
Dimitri Wolf <git@dimitri-wolf.de>
2020-01-26 17:04:43 +0100

    move activation functions to new plugin structure remove uninstall_v2.php

--- includes/class-woo-title-limit-activator.php
+++ includes/class-woo-title-limit-activator.php
@@ -30,7 +30,15 @@ class Woo_Title_Limit_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+        add_option('wtl_count_shop', 100);
+        add_option('wtl_count_category', 100);
+        add_option('wtl_count_product', 100);
+        add_option('wtl_count_home', 100);
+        add_option('wtl_checkbox_etc_shop', 0);
+        add_option('wtl_checkbox_etc_category', 0);
+        add_option('wtl_checkbox_etc_product', 0);
+        add_option('wtl_checkbox_widgets', 0);
+        add_option('wtl_checkbox_wordcutter', 0);
 	}
 
 }
--- woo-title-limit_v2.php
+++ woo-title-limit_v2.php
@@ -30,12 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'WOO_TITLE_LIMIT_VERSION', '1.0.0' );
+define( 'WOO_TITLE_LIMIT_VERSION', '2.0.0' );
 
 /**
  * The code that runs during plugin activation.
